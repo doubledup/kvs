4800c302a379285f884eb05b6202be7fb49595c9
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-04-22 15:03:57 +0200

    Implement enough for CLI tests to pass
    Except cli_version, because for some reason clap's `version` magic
    attribute prints to stderr, instead of stdout as the test requires. Any
    attempts to manually define a version flag cause only the application
    name to be printed to stderr. Flags not called `version` work just fine
    though.
    
    There might be a way forward with the Builder API, or another attribute
    to configure where the version information is directed.

--- src/bin/kvs.rs
+++ src/bin/kvs.rs
@@ -1,3 +1,50 @@
+use std::process::exit;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(version, about)]
+struct Cli {
+    #[clap(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// Set a key to a value
+    Set { key: String, value: String },
+
+    /// Get a value for a key
+    Get { key: String },
+
+    /// Remove a value for a key
+    Rm { key: String },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if let Some(cmd) = &cli.command {
+        match cmd {
+            Commands::Set {
+                key: _key,
+                value: _value,
+            } => {
+                // println!("Setting key `{}` to `{}`", key, value);
+                eprintln!("unimplemented");
+                exit(1);
+            }
+            Commands::Get { key: _key } => {
+                // println!("Getting value for key `{}`", key);
+                eprintln!("unimplemented");
+                exit(1);
+            }
+            Commands::Rm { key: _key } => {
+                // println!("Removing value at key `{}`", key);
+                eprintln!("unimplemented");
+                exit(1);
+            }
+        }
+    }
+
 }
