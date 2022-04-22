51dc7c753bcc2382caa6f8da7892aa6cdad74c89
David Dunn <26876072+doubledup@users.noreply.github.com>
2022-04-22 15:24:32 +0200

    Implement key-value store

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,26 @@
-pub struct KvStore {}
+use std::collections::HashMap;
+
+pub struct KvStore {
+    store: HashMap<String, String>,
+}
 
 impl KvStore {
     pub fn new() -> KvStore {
-        KvStore {}
+        KvStore {
+            store: HashMap::new()
+        }
     }
 
-    pub fn get(&self, _key: String) -> Option<String> {
-        unimplemented!();
+    pub fn get(&self, key: String) -> Option<String> {
+        let val = self.store.get(&key)?;
+        Some((*val).clone())
     }
 
-    pub fn set(&mut self, _key: String, _value: String) {
-        unimplemented!();
+    pub fn set(&mut self, key: String, value: String) {
+        self.store.insert(key, value);
     }
 
-    pub fn remove(&mut self, _key: String) {
-        unimplemented!();
+    pub fn remove(&mut self, key: String) {
+        self.store.remove(&key);
     }
 }
